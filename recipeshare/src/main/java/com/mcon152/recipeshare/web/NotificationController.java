c374270923ff7ee9eebb1ba1ad8744299061ee94
vennakag <vennakag@student.touro.edu>
2025-12-31 14:31:09 -0500

    Create NotificationController.java

---
+++ recipeshare/src/main/java/com/mcon152/recipeshare/web/NotificationController.java
@@ -0,0 +1,27 @@
+package com.mcon152.recipeshare.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.management.Notification;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/notifications")
+public class NotificationController {
+
+    private final NotificationService notificationService;
+
+    public NotificationController(NotificationService notificationService) {
+        this.notificationService = notificationService;
+    }
+
+    @GetMapping
+    public List<Notification> getNotifications(
+            @RequestParam Long userId
+    ) {
+        return notificationService.getNotificationsForUser(userId);
+    }
+}
